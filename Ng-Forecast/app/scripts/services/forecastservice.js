93f2316c78484f3d04be28f1526091963be2122a
Eric Lobdell <eric@ericlobdell.io>
2014-10-27 21:00:44 -0400

    Added weather icons
    Signed-off-by: Eric Lobdell <eric@ericlobdell.io>

--- Ng-Forecast/app/scripts/services/forecastservice.js
+++ Ng-Forecast/app/scripts/services/forecastservice.js
@@ -7,18 +7,18 @@
  * # forecastService
  * Service in the forecastApp.
  */
-angular.module('forecastApp')
-  .service('forecastService', function forecastService($http) {
+angular.module( 'forecastApp' )
+  .service( 'forecastService', function forecastService ( $http ) {
     var _apiKey = "fdad3018b75e8794ba88780f7d8c75d2",
-      baseRequestUrl = "https://api.forecast.io/forecast/" + _apiKey;
+        baseRequestUrl = "https://api.forecast.io/forecast/" + _apiKey;
 
-    function getForecast(lat, lon) {
+    function getForecast ( lat, lon ) {
       var url = baseRequestUrl + "/" + lat + "," + lon + "?callback=JSON_CALLBACK";
-      console.log("Requesting " + url);
-      return $http.jsonp(url);
+      console.log( "Requesting " + url );
+      return $http.jsonp( url );
     }
 
     return {
       getForecast: getForecast
     }
-  });
+  } );
